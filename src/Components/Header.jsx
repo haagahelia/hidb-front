0d3df9930b240dfe1961bb2fcaf3383ed3915c5f
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-10-30 13:28:27 +0200

    Create Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import { Search, TextAlignJustify, Plane } from "lucide-react";
+export default function Header() {
+  return (
+    <div className="w-full h-60 bg-[#FFFFFF] flex flex-col justify-center">
+      <div className="w-full bbg-[#FFFFFF] h-15 flex flex-row justify-between items-center p-2">
+        <Plane />
+        <div className="flex flex-row justify-between w-17">
+          <Search />
+          <TextAlignJustify />
+        </div>
+      </div>
+
+      <div className="w-full h-30 bg-[#FFFFFF] p-2 ">
+        <h1 className="text-2xl font-semibold">Welcome to Plane Museum.</h1>
+        <h1 className="text-2xl font-semibold">Scan for more.</h1>
+        <p className="text-[11px] font-regular">Today 9:00 - 17:00</p>
+      </div>
+    </div>
+  );
+}
