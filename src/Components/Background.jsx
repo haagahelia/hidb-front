b47730667a99bf33eacd7a12e9e4ec6f0d9c87a4
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-10-30 13:32:39 +0200

    add Header and List to Background

---
+++ src/Components/Background.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import List from "./List";
+
+export default function Background() {
+  return (
+    <div className="min-h-screen w-full bg-[#FFFFFF]">
+      <Header />
+      <List />
+    </div>
+  );
+}
