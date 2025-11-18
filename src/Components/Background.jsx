e956ba67a8e7e666617380f140c5b465abb30a52
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-11-18 15:43:57 +0200

    Add navigation bar to Screen

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import List from "./List";
-
+import NavBar from "./NavBar";
 export default function Background() {
   return (
     <div className="min-h-screen w-full bg-[#FFFFFF]">
       <Header />
       <List />
+      <NavBar />
     </div>
   );
 }
