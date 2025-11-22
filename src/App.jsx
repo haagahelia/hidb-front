99b64cb307e68cd9d9cfa04020788dda304e41d8
Anh Tran <bhm349@myy.haaga-helia.fi>
2025-11-22 22:05:01 +0200

    add index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import HomeScreen from "./Components/HomeScreen";
-import ScanScreen from "./Components/ScanScreen";
-import Map from "./Components/Map";
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/home" element={<HomeScreen />} />
-        <Route path="/scan" element={<ScanScreen />} />
-        <Route path="/map" element={<Map />} />
-      </Routes>
-    </>
-  );
-}
+import {TooltipProvider} from "@/components/ui/tooltip";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Index from "./pages/Index";
+// import Aircraft from "./pages/Aircraft";
+// import AircraftDetail from "./pages/AircraftDetail";
+// import MuseumMap from "./pages/MuseumMap";
+// import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+    <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                    {/* <Route path="/aircraft" element={<Aircraft />} />
+                    <Route path="/aircraft/:id" element={<AircraftDetail />} />
+                    <Route path="/map" element={<MuseumMap />} />
+                    <Route path="/about" element={<About />} /> */}
+                    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </TooltipProvider>
+    </QueryClientProvider>
+);
 
 export default App;
---
+++ src/types/images.d.ts
@@ -0,0 +1,16 @@
+declare module "*.jpg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.png" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.svg" {
+    import * as React from "react";
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
