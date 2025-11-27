0cf72f21eef5d6cd0ca01951f7a7d3e8356e8185
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-11-27 02:43:55 +0200

    create fetch service

---
+++ src/services/fetch.ts
@@ -0,0 +1,84 @@
+import { AircraftInterface } from "@/interfaces/interfaces"
+import { OrganizationInterface } from "@/interfaces/interfaces"
+ export async function fetchAllAirCrafts() {
+   
+    try {
+        const response = await fetch("http://localhost:4678/api/aircraft")
+
+        if (!response.ok) {
+            throw new Error (`HTTP error! Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+        const allAirCrafts: AircraftInterface[] = data.data
+        
+        return allAirCrafts;
+        
+    } catch (error) {
+        console.error('There was a problem:', error)
+        throw error;
+    }
+}
+
+ export async function fetchAircraftById(id: number) {
+   
+    try {
+        const response = await fetch(`http://localhost:4678/api/aircraft/${id}`)
+
+        if (!response.ok) {
+            throw new Error (`HTTP error! Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+        const singleAircraft: AircraftInterface = data.data
+        console.log(singleAircraft)
+        
+        return singleAircraft;
+        
+    } catch (error) {
+        console.error('There was a problem:', error)
+        throw error;
+    }
+}
+
+export async function fetchAllOrganizations() {
+   
+    try {
+        const response = await fetch("http://localhost:4678/api/organizations")
+
+        if (!response.ok) {
+            throw new Error (`HTTP error! Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+        const allOrganizations: OrganizationInterface[] = data.data
+        
+        return allOrganizations;
+        
+    } catch (error) {
+        console.error('There was a problem:', error)
+        throw error;
+    }
+}
+
+
+ export async function fetchOrganizationById(id: number) {
+   
+    try {
+        const response = await fetch(`http://localhost:4678/api/organizations/${id}`)
+
+        if (!response.ok) {
+            throw new Error (`HTTP error! Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+        const singleOrganization: OrganizationInterface = data.data
+        console.log(singleOrganization)
+        
+        return singleOrganization;
+        
+    } catch (error) {
+        console.error('There was a problem:', error)
+        throw error;
+    }
+}
