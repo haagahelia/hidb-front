1f49ac19376850a01efe18a19c57aea076ff0090
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2025-11-27 02:43:34 +0200

    Create interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,42 @@
+export interface AircraftInterface {
+  id: number;
+  name: string;
+  manufacturer: string;
+  model: string;
+  year_built: number;
+  weight: number;
+  organization_id: number | null;
+  crew_capacity: number | null;
+  passenger_capacity: number | null;
+  aircraft_type: string | null;
+  museum_location_number: number | null;
+  display_section: string | null;
+  qr_code_url: string | null;
+  description: string | null;
+  status: "on display" | "in storage" | "under restoration" | "loaned" | "decommissioned";
+  image: string;
+  specifications?: {
+    length?: string;
+    wingspan?: string;
+    height?: string;
+    maxSpeed?: string;
+    range?: string;
+    engine?: string;
+    armament?: string;
+  };
+  history?: string;
+  importance?: string;
+  funFacts?: string[];
+}
+
+export interface OrganizationInterface {
+    id: number;
+    name: string;
+    type: "airline" | "military" | "border_guard" | "postal_service" | "commercial" | "other";
+    country: string;
+    founding_year: number | null;
+    logo_url: string | null;
+    description?: string;
+    history?: string;
+    notable_aircraft?: number[];
+}
